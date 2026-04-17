1d4cc654e1ee426203392d6bb1af73bf9fbb2a26
morgan <morgan.le.chenechal@gmail.com>
2026-04-17 15:50:21 +0200

    comment useless startup call and disable sanitycheck for builds (to optimize a bit startup time)

--- Z-Anatomy PC/Assets/Scripts/Management/GlobalVariables.cs
+++ Z-Anatomy PC/Assets/Scripts/Management/GlobalVariables.cs
@@ -183,7 +183,9 @@ public class GlobalVariables : MonoBehaviour
             }
         }
 
+#if UNITY_EDITOR
         StartCoroutine(SanityCheck());
+#endif
         StartCoroutine(UrlNavidSelection());
     }
 
